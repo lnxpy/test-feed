f1c69a1d0459440a50fea782f0114cf85c16b6a0
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2022-04-16 14:51:18 +0430

    Update settings.py

--- feed_updater/settings.py
+++ feed_updater/settings.py
@@ -3,7 +3,7 @@ all configutation variables are stored in this file
 '''
 
 config = {
-        'endpoint': 'https://alirezayahyapour.pythonanywhere.com/api/v1/posts',
+    'endpoint': 'https://alirezayahyapour.pythonanywhere.com/api/v1/posts',
     'title': 'title',
     'url': {
         'pattern': 'https://alirezayahyapour.pythonanywhere.com/{}/{}',
